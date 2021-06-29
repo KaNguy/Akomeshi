616d1844df561323dfe1a63abbd163825dfbd004
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-06-29 16:51:41 -0700

    Feature: Event emitter pair for helping emit events

---
+++ src/main/scala/utility/Pair.scala
@@ -0,0 +1,17 @@
+package org.akomeshi
+package utility
+
+/**
+ * Created by KaNguy - 6/29/2021
+ * File utility/Pair.scala
+ */
+
+class Pair[T, U](T: T, U: U) {
+  def getT(): T = {
+    return T
+  }
+
+  def getU(): U = {
+    return U
+  }
+}
