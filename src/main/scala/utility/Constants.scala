670dd5c264166e8c544fd56b43983cb7c2ab223a
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-06 18:06:02 -0700

    Fix: Added zlib suffix constant

--- src/main/scala/utility/Constants.scala
+++ src/main/scala/utility/Constants.scala
@@ -24,4 +24,6 @@ case object Constants {
   val $os: String = System.getProperty("os.name")
   val $browser: String = "Akomeshi"
   val $device: String = "Akomeshi"
+
+  val zlibSuffix: Int = 0x0000FFFF
 }
