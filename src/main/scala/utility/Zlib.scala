e750c0bb902dc8e70425b84c1036f5795afbf120
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-06 19:33:23 -0700

    Feature: Completed the method for checking whether the incoming messages have the zlib suffix

--- src/main/scala/utility/Zlib.scala
+++ src/main/scala/utility/Zlib.scala
@@ -7,10 +7,15 @@ package utility
  */
 
 object Zlib {
-  def isFlushed(bytes: Array[Byte], offset: Int): Int = {
-    bytes(offset + 3) & 0xFF |
+  def isFlushed(bytes: Array[Byte]): Boolean = {
+    if (bytes.length < 4) return false
+    val offset: Int = bytes.length - 4
+
+    val zlibSuffix = bytes(offset + 3) & 0xFF |
       (bytes(offset + 2) & 0xFF) << 8 |
       (bytes(offset + 1) & 0xFF) << 16 |
       (bytes(offset) & 0xFF) << 24
+
+    zlibSuffix == Constants.zlibSuffix
   }
 }
