4c1af799b29d8ce0c82f11dc2c386326ac924dc9
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-07 14:00:10 -0700

    Fix: Code clean-up and added a latch await for a very small amount of time

--- src/main/scala/websocket/AkoWebSocket.scala
+++ src/main/scala/websocket/AkoWebSocket.scala
@@ -7,45 +7,37 @@ package websocket
  */
 
 // Networking & Web
-import java.net.http.{HttpClient, HttpHeaders, HttpRequest, HttpResponse, WebSocket}
-import java.net.http.WebSocket.{Builder, Listener}
-import java.net.{ConnectException, URI}
+import java.net.http.{HttpClient, WebSocket}
+import java.net.http.WebSocket.Listener
+import java.net.URI
 
 // New I/O
 import java.nio.ByteBuffer
-import java.nio.CharBuffer
+import java.nio.charset.StandardCharsets
 
 // Utilities
-import java.util.concurrent.{CompletableFuture, CompletionStage, CountDownLatch, TimeUnit}
+import java.util.concurrent.{CompletableFuture, CountDownLatch, TimeUnit}
 
-// Local
-import org.akomeshi.websocket.WebSocketListener
-
-class AkoWebSocket(var url: String = null, var listener: Listener = new WebSocketListener(), connectionTimeout: Int = 1000) {
+class AkoWebSocket(var url: String = null, var listener: Listener = WebSocketListener(), connectionTimeout: Int = 1000) {
   private val hasWSProtocol: Boolean = this.hasWebSocketProtocol(url)
   if (!hasWSProtocol) throw new Error("The URL does not have a WebSocket protocol")
 
   private val latch: CountDownLatch = new CountDownLatch(1)
 
   private val httpClient: HttpClient = HttpClient.newHttpClient()
-  private var webSocket: WebSocket = httpClient.newWebSocketBuilder().buildAsync(URI.create(url), listener).join()
+  private val webSocket: WebSocket = httpClient.newWebSocketBuilder().buildAsync(URI.create(url), listener).join()
+
+  latch.await(this.connectionTimeout / 10, TimeUnit.MILLISECONDS)
 
   val subprotocol: String = webSocket.getSubprotocol
 
   def interact(action: String = null, data: CharSequence = null, message: ByteBuffer = null, statusCode: Int = WebSocket.NORMAL_CLOSURE, reason: String = "", last: Boolean = false, timeout: Int = 1000): Unit = {
     action.toUpperCase match {
-      case "SEND" => {
-        this.webSocket.sendText(data, last)
-      } case "CLOSE" => {
-        this.webSocket.sendClose(statusCode, reason)
-      } case "PING" => {
-        this.webSocket.sendPing(message)
-      } case "PONG" => {
-        this.webSocket.sendPong(message)
-      } case "BINARY" => {
-        // Sends binaries, untested
-        this.webSocket.sendBinary(ByteBuffer.allocate(CharBuffer.wrap(data).length() * Character.BYTES), last)
-      }
+      case "SEND" => this.webSocket.sendText(data, last)
+      case "CLOSE" => this.webSocket.sendClose(statusCode, reason)
+      case "PING" => this.webSocket.sendPing(message)
+      case "PONG" => this.webSocket.sendPong(message)
+      case "BINARY" => this.webSocket.sendBinary(ByteBuffer.wrap(data.toString.getBytes(StandardCharsets.UTF_8)), last)
       case _ => ()
     }
     latch.await(timeout, TimeUnit.MILLISECONDS)
