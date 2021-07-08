a9305362410235fbd7296742bce9b0e31b44690b
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-08 16:06:33 -0700

    Feature: Heartbeat fully works

--- src/main/scala/core/Heartbeat.scala
+++ src/main/scala/core/Heartbeat.scala
@@ -10,13 +10,15 @@ package core
 import websocket.AkoWebSocket
 
 // Utility
-import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.{ScheduledExecutorService, TimeUnit, Executors}
 
 // JSON
 import json.JSONString
 
 object Heartbeat {
-  def sendHeartbeat(heartbeat_interval: Int, executor: ScheduledExecutorService, connection: AkoWebSocket): Unit = {
-    connection.send(JSONString.encode(PayloadModels.heartbeatPayload), last = true)
+  def sendHeartbeat(heartbeat_interval: Int, executor: ScheduledExecutorService = Executors.newScheduledThreadPool(1), connection: AkoWebSocket): Unit = {
+    executor.scheduleAtFixedRate(() => {
+      connection.send(JSONString.encode(PayloadModels.heartbeatPayload), last = true)
+    }, 0, heartbeat_interval, TimeUnit.MILLISECONDS)
   }
 }
