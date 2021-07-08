12ec5f3db9b9c8e968f104d43d2d74381c9b76ce
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-08 15:41:08 -0700

    Feature: Heartbeat payload model

--- src/main/scala/core/PayloadModels.scala
+++ src/main/scala/core/PayloadModels.scala
@@ -10,10 +10,12 @@ package core
 import org.akomeshi.utility.Constants
 
 object PayloadModels {
-  val identifyPayload: String => Map[Any, Any] = (token) => Map("op" -> 2, "d" ->
-    Map("token" -> s"Bot ${token}", "intents" -> 513 /* Default intents, will change this later */, "properties" ->
+  val identifyPayload: String => Map[Any, Any] = token => Map("op" -> 2, "d" ->
+    Map("token" -> s"Bot $token", "intents" -> 513 /* Default intents, will change this later */, "properties" ->
       Map("$os" -> Constants.$os, "$browser" -> Constants.$browser, "$device" -> Constants.$device
       )
     )
   )
+
+  val heartbeatPayload: Map[String, Int] = Map("op" -> 10)
 }
