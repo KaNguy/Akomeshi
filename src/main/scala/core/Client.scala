50633f3afdbbd12b0c3db419b92836a94bee37b3
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-09 22:14:39 -0700

    Feature: Client class, will do the logins

---
+++ src/main/scala/core/Client.scala
@@ -0,0 +1,18 @@
+package org.akomeshi
+package core
+
+/**
+ * Created by KaNguy - 07/09/2021
+ * File core/Client.scala
+ */
+
+// Akomeshi
+import json.JSONString
+
+class Client {
+  val universalGatewayClass: Gateway = new Gateway()
+
+  def login(token: String): Unit = {
+    this.universalGatewayClass.connection.send(JSONString.encode(PayloadModels.identifyPayload(token)), last = true, timeout = 100)
+  }
+}
