8bc8b55d891552247fe30539f920fd903c7faebf
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-15 11:40:17 -0700

    Feature: Library will need to make requests to the Discord API and get the output of the request, making an output reader, reused from my previous implementation

---
+++ src/main/scala/core/api/request/RequestOutput.scala
@@ -0,0 +1,20 @@
+package org.akomeshi
+package core.api.request
+
+/**
+ * Created by KaNguy - 6/30/2021
+ * File core/api/request/Response.scala
+ */
+
+// Input / Output
+import java.io.{InputStream, InputStreamReader, Reader}
+
+// Networking
+import java.net.HttpURLConnection
+
+// Compression / Decompression
+import java.util.zip.{GZIPOutputStream, DeflaterOutputStream}
+
+object RequestOutput {
+
+}
