078d5eca9bc35c86d65721d2ed414abff54e38c6
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-21 11:44:55 -0700

    Feature: Add a logout method

--- src/main/scala/core/Client.scala
+++ src/main/scala/core/Client.scala
@@ -9,6 +9,9 @@ package core
 // Akomeshi
 import json.JSONString
 
+// WebSocket
+import java.net.http.WebSocket
+
 class Client {
   val universalGatewayClass: Gateway = new Gateway()
 
@@ -16,5 +19,10 @@ class Client {
     // TODO: Work on this a little more
     this.universalGatewayClass.connection.send(JSONString.encode(PayloadModels.identifyPayload(token)), last = true)
     Heartbeat.sendHeartbeat(41250, this.universalGatewayClass.connection)
+  }
+
+  def logout(): Unit = {
+    this.universalGatewayClass.connection.close(WebSocket.NORMAL_CLOSURE, "Normal closure", timeout = 0)
+    if (!Heartbeat.executor.isTerminated) Heartbeat.executor.shutdown()
   }
 }
