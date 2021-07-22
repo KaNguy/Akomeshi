7ecedddbb22403e82611f1c55fb7a07d1dd48c64
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-22 16:06:20 -0700

    Feature: Request object has its own request method, will be testing it later.

--- src/main/scala/core/api/request/Request.scala
+++ src/main/scala/core/api/request/Request.scala
@@ -6,6 +6,24 @@ package core.api.request
  * File core/Request.scala
  */
 
-class Request {
+// Networking & Web
+import java.net.URI
+import java.net.http.{HttpClient, HttpRequest, HttpResponse}
 
+object Request {
+  def request(url: String, method: String = RequestConstants.GET, headers: Iterable[(String, String)], data: String = null): String = {
+    val client: HttpClient = HttpClient.newHttpClient()
+
+    val request: HttpRequest.Builder = HttpRequest.newBuilder(URI.create(url))
+      .method(
+        if (RequestConstants.requestMethods.contains(method.toUpperCase)) method.toUpperCase else RequestConstants.GET,
+        if (method.toUpperCase.equals(RequestConstants.GET)) HttpRequest.BodyPublishers.noBody() else HttpRequest.BodyPublishers.ofString(if (data == null) "" else data)
+      )
+      .version(HttpClient.Version.HTTP_2)
+
+    if (headers.nonEmpty) headers.foreach(k => request.header(k._1, k._2))
+
+    val response: HttpResponse[String] = client.send(request.build(), HttpResponse.BodyHandlers.ofString())
+    response.body
+  }
 }
