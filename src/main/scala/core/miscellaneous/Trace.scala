5e7dc9cd00bae40b60d5f5468dc5bf1bbae4a9c0
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-21 22:14:07 -0700

    Feature: Trace class for debug logging, will make a different way to debug later

---
+++ src/main/scala/core/miscellaneous/Trace.scala
@@ -0,0 +1,11 @@
+package org.akomeshi
+package core.miscellaneous
+
+/**
+ * Created by KaNguy - 07/21/2021
+ * File miscellaneous/Trace.scala
+ */
+
+object Trace {
+  val logger: Logger = Logger(log = true)
+}
