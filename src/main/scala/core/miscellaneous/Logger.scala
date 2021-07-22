27a07d194f85c9251bc0e7e7056e7f99da8fcf21
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-21 22:11:16 -0700

    Feature: Logger class for debugging

---
+++ src/main/scala/core/miscellaneous/Logger.scala
@@ -0,0 +1,21 @@
+package org.akomeshi
+package core.miscellaneous
+
+/**
+ * Created by KaNguy - 07/21/2021
+ * File miscellaneous/Logger.scala
+ */
+
+case class Logger(log: Boolean = false) {
+  private def check(callback: () => Unit): Unit = {
+    if (this.log) callback()
+  }
+
+  def debug(x: String*): Unit = {
+    this.check(() => println(x.mkString("", ", ", "")))
+  }
+
+  def show(x: Any): Unit = {
+    this.check(() => println(x))
+  }
+}
