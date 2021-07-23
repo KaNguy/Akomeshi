167739bb6f6c02f5319218674be27a6298682443
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-22 20:01:17 -0700

    Documentation: Correct file header

--- src/main/scala/core/api/request/RequestConstants.scala
+++ src/main/scala/core/api/request/RequestConstants.scala
@@ -1,6 +1,11 @@
 package org.akomeshi
 package core.api.request
 
+/**
+ * Created by KaNguy - 07/22/2021
+ * File core/api/request/RequestConstants.scala
+ */
+
 case object RequestConstants {
   val GET: String = "GET"
   val POST: String = "POST"
