221fb2d88aa73f8196ffb729c2b33a714b322b87
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-24 18:38:07 -0700

    Feature: Event manager now uses a much simpler approach, still in progress; removed the dispatch method as it was not needed.

--- src/main/scala/core/structures/EventManager.scala
+++ src/main/scala/core/structures/EventManager.scala
@@ -8,30 +8,20 @@ package core.structures
 
 // Akomeshi
 import event.EventObjects
+import utility.Constants
 
 // Utilities
 import java.util
 
 case class EventManager(/*eventData: Iterable[(Any, Any)]*/) {
   EventObjects.mapEmitter.on("WS_MESSAGE", (_, data) => {
-    if (data.getOrElse("t", "t") != null && data.getOrElse("t", "t").isInstanceOf[String]) {
-      EventObjects.hashMapEmitter.emit("D_MESSAGE", data.getOrElse("d", "d").asInstanceOf[util.HashMap[Any, Any]])
+    if (
+      data("t") != null &&
+      data("t").isInstanceOf[String] &&
+      data("op") != null &&
+      data("op") == Constants.GatewayOpcodes("DISPATCH")
+    ) {
+        EventObjects.hashMapEmitter.emit("D_MESSAGE", data.getOrElse("d", "d").asInstanceOf[util.HashMap[Any, Any]])
     }
   })
-
-  // TODO: Don't actually use this
-  def dispatch(event: Iterable[(Any, Any)]): util.HashMap[Any, Any] = {
-    if (event.toMap.getOrElse("t", "t") != null &&
-      event.toMap.getOrElse("d", "d") != null &&
-      event.toMap.getOrElse("op", "op") != null &&
-      event.toMap.getOrElse("op", "op") == 0) event.toMap.getOrElse("d", "d").asInstanceOf[util.HashMap[Any, Any]]
-    else {
-      val eventMap = new util.HashMap[Any, Any]()
-      eventMap.put("op", 0)
-      eventMap.put("s", null)
-      eventMap.put("t", null)
-      eventMap.put("d", null)
-      eventMap
-    }
-  }
 }
