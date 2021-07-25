36b5d058d4a50479abacf05e0a452665d1c5d58c
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-25 08:47:58 -0700

    Experimental Feature: Get content of the message

--- src/main/scala/core/structures/Message.scala
+++ src/main/scala/core/structures/Message.scala
@@ -12,7 +12,11 @@ import event.EventObjects
 // Utility
 import java.util
 
-case class Message() {
+case class Message(message: Map[Any, Any] = Map.empty[Any, Any]) {
+  def content: String = {
+    message("content").toString
+  }
+
   EventObjects.mapEmitter.on("WS_MESSAGE", (_, data) => {
     if (data.getOrElse("t", "t") != null && data.getOrElse("t", "t").toString.equals("MESSAGE_CREATE")) {
       EventObjects.hashMapEmitter.emit("D_MESSAGE", data.getOrElse("d", "d").asInstanceOf[util.HashMap[Any, Any]])
