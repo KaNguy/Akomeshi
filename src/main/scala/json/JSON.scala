7088732cbb80c203bdf10e727dac517c922009d1
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-25 17:35:34 -0700

    Feature: Added a parseAsHashMap method

--- src/main/scala/json/JSON.scala
+++ src/main/scala/json/JSON.scala
@@ -6,10 +6,14 @@ package json
  * File json/JSON.scala
  */
 
+// Utility
+import java.util
+
 // Convertors
 import scala.jdk.CollectionConverters._
 
 object JSON {
   def parse(json: String): Object = JSONParser.parse(json)
   def parseAsMap(json: String): Map[Any, Any] = JSONParser.parseAsMap(json).asScala.toMap
+  def parseAsHashMap(json: String): util.HashMap[Any, Any] = JSONParser.parseAsHashMap(json).asInstanceOf[util.HashMap[Any, Any]]
 }
