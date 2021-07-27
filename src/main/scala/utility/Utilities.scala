d0e2b4b15fde969af2aef10718375c85f58cb9e3
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-27 12:38:31 -0700

    Feature: General utilities file for utilities that do not fit anywhere in general.

---
+++ src/main/scala/utility/Utilities.scala
@@ -0,0 +1,12 @@
+package org.akomeshi
+package utility
+
+/**
+ * Created by KaNguy - 07/27/2021
+ * File utility/Utilities.scala
+ */
+
+case object Utilities {
+  def strToBool(x: String): Boolean = x.toBoolean
+  def strToInt(x: String): Int = x.toInt
+}
