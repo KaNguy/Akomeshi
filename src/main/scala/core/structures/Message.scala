54751183f6d7cb7386b4afe4ae0dc574e5c35907
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-26 22:12:16 -0700

    Feature: Private casting class to safely cast data such as Strings to Booleans or Strings to Integers

--- src/main/scala/core/structures/Message.scala
+++ src/main/scala/core/structures/Message.scala
@@ -12,8 +12,14 @@ import event.EventObjects
 // Utility
 import java.util
 
-case class Message(message: util.HashMap[Any, Any] = util.HashMap[Any, Any]) {
+case class Message(message: util.HashMap[Any, Any] = new util.HashMap[Any, Any]()) {
+  // TODO: Move this to a general utilities file
+  private def cast[X, Y](x: Any): Y = x.asInstanceOf[X].asInstanceOf[Y]
+
   def content: String = message.get("content").toString
+  def mentionsEveryone: Boolean = cast[String, Boolean](message.get("mention_everyone"))
+  def pinned: Boolean = cast[String, Boolean](message.get("pinned"))
+  def flags: Int = cast[String, Int](message.get("flags"))
 
   EventObjects.hashMapEmitter.on("WS_MESSAGE", (_, data) => {
     if (data.get("t") != null && data.get("t").toString.equals("MESSAGE_CREATE")) {
