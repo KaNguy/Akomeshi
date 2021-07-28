142f41618534035ca916684dbe6a86ffe2098ace
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-28 10:16:39 -0700

    Fix: Message case class no longer needs to use an event on its own

--- src/main/scala/core/structures/Message.scala
+++ src/main/scala/core/structures/Message.scala
@@ -18,10 +18,4 @@ case class Message(message: util.HashMap[Any, Any] = new util.HashMap[Any, Any](
   def mentionsEveryone: Boolean = Util.strToBool(message.get("mention_everyone").toString)
   def pinned: Boolean = Util.strToBool(message.get("pinned").toString)
   def flags: Int = Util.strToInt(message.get("flags").toString)
-
-  EventObjects.hashMapEmitter.on("WS_MESSAGE", (_, data) => {
-    if (data.get("t") != null && data.get("t").toString.equals("MESSAGE_CREATE")) {
-      EventObjects.hashMapEmitter.emit("D_MESSAGE", data.get("d").asInstanceOf[util.HashMap[Any, Any]])
-    }
-  })
 }
