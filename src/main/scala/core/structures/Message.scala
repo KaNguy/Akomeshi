36ca3df74699010c450d43a4bad3945812ac903a
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-28 17:57:32 -0700

    Fix: Remove the need to call .toString

--- src/main/scala/core/structures/Message.scala
+++ src/main/scala/core/structures/Message.scala
@@ -15,10 +15,10 @@ import java.util
 
 case class Message(message: util.HashMap[Any, Any] = new util.HashMap[Any, Any]()) {
   def content: String = message.get("content").toString
-  def mentionsEveryone: Boolean = Util.strToBool(message.get("mention_everyone").toString)
-  def pinned: Boolean = Util.strToBool(message.get("pinned").toString)
-  def flags: Int = Util.strToInt(message.get("flags").toString)
-  def messageType: Int = Util.strToInt(message.get("type").toString)
-  def nonce: Int = Util.strToInt(message.get("nonce").toString)
-  def isTTS: Boolean = Util.strToBool(message.get("tts").toString)
+  def mentionsEveryone: Boolean = Util.strToBool(message.get("mention_everyone"))
+  def pinned: Boolean = Util.strToBool(message.get("pinned"))
+  def flags: Int = Util.strToInt(message.get("flags"))
+  def messageType: Int = Util.strToInt(message.get("type"))
+  def nonce: Int = Util.strToInt(message.get("nonce"))
+  def isTTS: Boolean = Util.strToBool(message.get("tts"))
 }
