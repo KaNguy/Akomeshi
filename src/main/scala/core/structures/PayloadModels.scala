fc113b9bc7b723154677843b2b7f8cc2e7f56dc5
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-29 09:03:00 -0700

    Refactor: Move PayloadModels.scala to the structures package

---
+++ src/main/scala/core/structures/PayloadModels.scala
@@ -0,0 +1,15 @@
+package org.akomeshi
+package core.structures
+
+import utility.Constants
+
+object PayloadModels {
+  val identifyPayload: String => Map[Any, Any] = token => Map("op" -> 2, "d" ->
+    Map("token" -> s"Bot $token", "intents" -> 513 /* Default intents, will change this later */ , "properties" ->
+      Map("$os" -> Constants.$os, "$browser" -> Constants.$browser, "$device" -> Constants.$device
+      )
+    )
+  )
+
+  val heartbeatPayload: Map[String, Any] = Map("op" -> 1, "d" -> "null")
+}
