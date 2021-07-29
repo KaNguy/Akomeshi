d8b6cd011f996b4d9c0854ac51b72bdcdce40f90
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-28 17:54:06 -0700

    Feature: Add more message properties

--- src/main/scala/core/structures/Message.scala
+++ src/main/scala/core/structures/Message.scala
@@ -18,4 +18,7 @@ case class Message(message: util.HashMap[Any, Any] = new util.HashMap[Any, Any](
   def mentionsEveryone: Boolean = Util.strToBool(message.get("mention_everyone").toString)
   def pinned: Boolean = Util.strToBool(message.get("pinned").toString)
   def flags: Int = Util.strToInt(message.get("flags").toString)
+  def messageType: Int = Util.strToInt(message.get("type").toString)
+  def nonce: Int = Util.strToInt(message.get("nonce").toString)
+  def isTTS: Boolean = Util.strToBool(message.get("tts").toString)
 }
