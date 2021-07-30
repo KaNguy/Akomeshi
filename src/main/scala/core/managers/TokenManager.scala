8c9923b4d3ba1741cf32200d1219868522d2918e
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-30 10:58:52 -0700

    Documentation: Document TokenManager

--- src/main/scala/core/managers/TokenManager.scala
+++ src/main/scala/core/managers/TokenManager.scala
@@ -10,7 +10,14 @@ package core.managers
 import java.util
 
 object TokenManager {
+  // Storage for the token
   val storage: util.HashMap[String, String] = new util.HashMap[String, String]()
+
+  /**
+   * Pushes a key and value to the storage
+   * @param key Key
+   * @param token Token or value
+   */
   def push(key: String, token: String): Unit = this.storage.put(key, token)
 }
 
