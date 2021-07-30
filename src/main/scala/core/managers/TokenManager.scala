839c38e131d04008fa627be8f6a564025814b39f
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-30 11:13:52 -0700

    Feature: Add a getToken method to get the token only, not from the storage Documentation: Add documentation to the getToken method Fix: The storage val is now private

--- src/main/scala/core/managers/TokenManager.scala
+++ src/main/scala/core/managers/TokenManager.scala
@@ -11,13 +11,26 @@ import java.util
 
 object TokenManager {
   // Storage for the token
-  val storage: util.HashMap[String, String] = new util.HashMap[String, String]()
+  private val storage: util.HashMap[String, String] = new util.HashMap[String, String]()
 
   /**
    * Pushes a key and value to the storage
    * @param key Key
    * @param token Token or value
    */
-  def push(key: String, token: String): Unit = this.storage.put(key, token)
+  def push(key: String, token: String): Unit = {
+    // Prevents anything but the token from being pushed
+    if (!key.equals("token")) return
+    this.storage.put(key, token)
+  }
+
+  /**
+   * Gets the token since the storage is private
+   * @return Token String
+   */
+  def getToken: String = {
+    if (this.storage.get("token") != null) this.storage.get("token")
+    else null
+  }
 }
 
