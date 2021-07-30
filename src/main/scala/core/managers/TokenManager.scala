eca887e43cc7f82e9f590786987ab63eeffb233d
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-29 19:55:33 -0700

    Fix: Place the storage in the TokenManager

--- src/main/scala/core/managers/TokenManager.scala
+++ src/main/scala/core/managers/TokenManager.scala
@@ -1,9 +1,16 @@
 package org.akomeshi
 package core.managers
 
+/**
+ * Created by KaNguy - 07/29/2021
+ * File core/structures/TokenManager.scala
+ */
+
+// Utilities
 import java.util
 
 case class TokenManager(token: String) {
-  def push(token: String = this.token): Unit = TokenHolder.storage.put("token", token)
+  val storage: util.HashMap[String, String] = new util.HashMap[String, String]()
+  def push(key: String, token: String = this.token): Unit = this.storage.put(key, token)
 }
 
