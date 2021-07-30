48eec053ed12c836b9a0411b6ecc3d56fcbe76eb
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-30 10:21:32 -0700

    Documentation: Add documentation for `strToBool` and `strToInt` methods of the general utilities object

--- src/main/scala/utility/Utilities.scala
+++ src/main/scala/utility/Utilities.scala
@@ -7,6 +7,17 @@ package utility
  */
 
 case object Utilities {
+  /**
+   * Converts unknown data that is presumed to be a string into a boolean
+   * @param x Any, preferably a string-compatible parameter
+   * @return Boolean
+   */
   def strToBool(x: Any): Boolean = x.toString.toBoolean
+
+  /**
+   * Converts unknown data that is presumed to be a string into an integer
+   * @param x Any, preferably a string-compatible parameter
+   * @return Int
+   */
   def strToInt(x: Any): Int = x.toString.toInt
 }
