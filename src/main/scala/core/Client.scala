cd164a64448c4e8479d347fd1b3f9bc21fdad570
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-30 11:08:05 -0700

    Documentation: Add documentation for all methods so far in the client class

--- src/main/scala/core/Client.scala
+++ src/main/scala/core/Client.scala
@@ -16,17 +16,28 @@ import json.JSONString
 import java.net.http.WebSocket
 
 class Client(val token: String) {
+  // Starts dispatching events from the HashMap emitter
   EventDispatcher(EventObjects.hashMapEmitter)
+
+  // Pushes the token to the library's token storage in order to successfully authorize other actions the client wants to complete
   TokenManager.push("token", this.token)
 
   val universalGatewayClass: Gateway = new Gateway()
 
+  /**
+   * Opens the WebSocket connection, sends the identify payload, and starts sending heartbeats
+   * @param token Bot token, if not provided, the token parameter of the class will be used
+   */
   def login(token: String = this.token): Unit = {
-    // TODO: Work on this a little more
     this.universalGatewayClass.connection.send(JSONString.encode(PayloadModels.identifyPayload(token)), last = true)
+    if (TokenManager.storage.get("token") == null) TokenManager.push("token", token)
     Heartbeat.sendHeartbeat(41250, this.universalGatewayClass.connection)
   }
 
+  /**
+   * Closes the WebSocket connection with a normal closure and shuts down other tasks
+   * @param timeout Timeout, should at least be above 0 in some cases
+   */
   def logout(timeout: Int = 300): Unit = {
     this.universalGatewayClass.connection.close(WebSocket.NORMAL_CLOSURE, "Normal closure", timeout = timeout)
     if (!Heartbeat.executor.isTerminated) Heartbeat.executor.shutdown()
