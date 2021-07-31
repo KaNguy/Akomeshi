9b75d89023caf72334d646262f064c86ceba8500
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-31 15:00:11 -0700

    Feature (planned feature): RequestFrame for simplifying the client's requests.

---
+++ src/main/scala/core/api/request/RequestFrame.scala
@@ -0,0 +1,13 @@
+package org.akomeshi
+package core.api.request
+
+/**
+ * Created by KaNguy - 07/31/2021
+ * File core/api/request/RequestFrame.scala
+ */
+
+// TODO: Implement this
+// TODO: RequestFrame for predetermined requests such as authorized POST or GET
+object RequestFrame {
+
+}
