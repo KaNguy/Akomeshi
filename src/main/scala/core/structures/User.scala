c21185b33d3bdb7832a43a06a1e6c6c2b17a95c6
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-31 22:11:48 -0700

    Feature: Add `Self` case class and `self` method which gives basic properties of client's self

--- src/main/scala/core/structures/User.scala
+++ src/main/scala/core/structures/User.scala
@@ -6,7 +6,28 @@ package core.structures
  * File core/structures/User.scala
  */
 
+// Akomeshi
+import core.managers.Cache
+import utility.Utilities
+
+// Utilities
+import java.util
+
 // TODO: Implement a user class for getting the client's own user info
 case class User() {
+  var emptyReadyCache: Boolean = true
+  if (Cache.readyCache.get("READY") != null) {
+    this.emptyReadyCache = false
+  }
 
+  def self: Self = Self(Utilities.toHashMap(Cache.readyCache.get("READY").asInstanceOf[util.HashMap[Any, Any]].get("user")))
+
+  case class Self(user: util.HashMap[Any, Any]) {
+    def bot: Boolean = Utilities.strToBool(user.get("bot"))
+    def verified: Boolean = Utilities.strToBool(user.get("verified"))
+    def id: String = user.get("id").toString
+    def avatarHash: String = user.get("avatar").toString
+    def username: String = user.get("username").toString
+    def discriminator: String = user.get("discriminator").toString
+  }
 }
