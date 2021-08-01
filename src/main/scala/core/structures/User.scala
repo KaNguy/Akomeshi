6b35c102b2d6235bab8693b9f3e3ff93e99d19a7
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-07-31 19:50:03 -0700

    Feature (planned): User property

---
+++ src/main/scala/core/structures/User.scala
@@ -0,0 +1,12 @@
+package org.akomeshi
+package core.structures
+
+/**
+ * Created by KaNguy - 07/31/2021
+ * File core/structures/User.scala
+ */
+
+// TODO: Implement a user class for getting the client's own user info
+case class User() {
+
+}
