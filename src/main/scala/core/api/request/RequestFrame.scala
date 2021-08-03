b0a5cffaadc1160f467ce47379b29d75cb60cab8
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-08-02 17:00:05 -0700

    Feature: Add `post` method to the RequestFrame object

--- src/main/scala/core/api/request/RequestFrame.scala
+++ src/main/scala/core/api/request/RequestFrame.scala
@@ -6,8 +6,31 @@ package core.api.request
  * File core/api/request/RequestFrame.scala
  */
 
+// Akomeshi
+import core.managers.TokenManager
+import utility.Constants
+import json.JSONString
+
 // TODO: Implement this
 // TODO: RequestFrame for predetermined requests such as authorized POST or GET
 object RequestFrame {
-
+  /**
+   * Makes a POST request on the client's behalf with the needed authorization headers
+   * @param url URL
+   * @param data Data as a Map
+   * @return Output as a String
+   */
+  def post(url: String, data: Map[Any, Any]): String = {
+    Request.request(
+      url,
+      RequestConstants.POST,
+      Map(
+        "Authorization" -> s"Bot ${TokenManager.getToken}",
+        "Content-Type" -> "application/json",
+        "User-Agent" -> Constants.userAgent,
+        "Accept" -> "*/*"
+      ),
+      JSONString.encode(data),
+    )
+  }
 }
