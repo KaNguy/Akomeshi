f1844f00a9f936dfc579b4a0c77a7968f3ec2809
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-08-11 10:48:16 -0700

    Documentation: Method, `delete` has been documented.

--- src/main/scala/core/api/request/RequestFrame.scala
+++ src/main/scala/core/api/request/RequestFrame.scala
@@ -51,7 +51,11 @@ object RequestFrame {
     )
   }
 
-  // TODO: Document this
+  /**
+   * Makes a DELETE rquest with the client's needed authorization headers
+   * @param url - URL
+   * @return Output as a String
+   */
   def delete(url: String): String = {
     Request.request(
       url = url,
