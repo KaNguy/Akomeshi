00d0990e595c91cef6b00dc700819658567c7ed1
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-08-12 09:57:25 -0700

    Documentation: Added comments to the payload structures.

--- src/main/scala/core/structures/PayloadModels.scala
+++ src/main/scala/core/structures/PayloadModels.scala
@@ -10,6 +10,7 @@ package core.structures
 import utility.Constants
 
 object PayloadModels {
+  // Identification payload to the gateway.
   val identifyPayload: (String, Int) => Map[Any, Any] = (token, intents) => Map("op" -> 2, "d" ->
     Map("token" -> s"Bot $token", "intents" -> intents, "properties" ->
       Map("$os" -> Constants.$os, "$browser" -> Constants.$browser, "$device" -> Constants.$device
@@ -17,8 +18,10 @@ object PayloadModels {
     )
   )
 
+  // Heartbeat payload to keep the connection alive.
   val heartbeatPayload: Map[String, Any] = Map("op" -> 1, "d" -> "null")
 
+  // Resume payload in case the connection needs to be resumed.
   val resumePayload: (String, String) => Map[Any, Any] = (token, sessionID) => Map("op" -> 6, "d" ->
     Map("token" -> s"Bot $token", "session_id" -> sessionID, "seq" -> 7777)
   )
