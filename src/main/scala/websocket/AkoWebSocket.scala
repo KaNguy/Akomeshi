12a66cee184b30da18df851fca556aac3541fae1
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-08-17 09:23:24 -0700

    Documentation: Documented the `pong` method.

--- src/main/scala/websocket/AkoWebSocket.scala
+++ src/main/scala/websocket/AkoWebSocket.scala
@@ -100,6 +100,12 @@ class AkoWebSocket(var url: String = null, var listener: Listener = WebSocketLis
     }
   }
 
+  /**
+   * Pongs the WebSocket
+   * @param message Message in the form of a ByteBuffer.
+   * @param timeout Latch timeout
+   * @return CompletableFuture that does not need to be used.
+   */
   def pong(message: ByteBuffer, timeout: Int = 1000): CompletableFuture[AkoWebSocket] = {
     try {
       this.webSocket.sendPong(message)
