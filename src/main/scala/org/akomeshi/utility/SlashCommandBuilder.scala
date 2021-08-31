026b3f92231558c7edd1818db10039cef19263c3
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-08-31 09:55:37 -0700

    Feature: Add Application Command types.

--- src/main/scala/org/akomeshi/utility/SlashCommandBuilder.scala
+++ src/main/scala/org/akomeshi/utility/SlashCommandBuilder.scala
@@ -7,5 +7,16 @@ package utility
  */
 
 object SlashCommandBuilder {
+  /**
+   * Application command types.
+   * @see [[https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-types]]
+   */
+  type CommandType = Int
+  val CHAT_INPUT: CommandType = 1
+  val USER: CommandType = 1
+  val MESSAGE: CommandType = 3
 
+  def createSlashCommand(name: String, commandType: Int): Map[String, Any] = {
+    Map("" -> "")
+  }
 }
