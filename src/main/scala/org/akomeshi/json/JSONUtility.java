64f5b1ca414e5ebf4eaaef311495fd2dc3d5d7c4
Yen <68307546+KaNguy@users.noreply.github.com>
2021-10-15 10:42:52 -0700

    Documentation: Documented all methods.

--- src/main/scala/org/akomeshi/json/JSONUtility.java
+++ src/main/scala/org/akomeshi/json/JSONUtility.java
@@ -6,14 +6,29 @@ package org.akomeshi.json;
  */
 
 public class JSONUtility {
+    /**
+     * Checks if a character is a whitespace character.
+     * @param character Character.
+     * @return Boolean.
+     */
     public static boolean isWhitespace(char character) {
         return character == ' ' || character == '\n' || character == '\t';
     }
 
+    /**
+     * Checks if a character is a letter.
+     * @param character Character.
+     * @return Boolean.
+     */
     public static boolean isLetter(char character) {
         return character >= 'a' && character <= 'z';
     }
 
+    /**
+     * Checks if a character is a number.
+     * @param character Character.
+     * @return Boolean.
+     */
     public static boolean isNumberStarter(char character) {
         return (character >= '0' && character <= '9') || character == '-';
     }
