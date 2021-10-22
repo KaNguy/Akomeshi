c259bae617913028be2520aec9fbd39cf5b07799
Yen <68307546+KaNguy@users.noreply.github.com>
2021-10-22 08:57:34 -0700

    Fix / Revamp: HashMaps will now be strictly typed as HashMap[String, Any] to ensure that the keys are strings.

--- src/main/scala/org/akomeshi/core/structures/PayloadModels.scala
+++ src/main/scala/org/akomeshi/core/structures/PayloadModels.scala
@@ -12,7 +12,7 @@ import org.akomeshi.utility.Constants
 
 object PayloadModels {
   // Identification payload to the gateway.
-  val identifyPayload: (String, Int) => Map[Any, Any] = (token, intents) => Map("op" -> 2, "d" ->
+  val identifyPayload: (String, Int) => Map[String, Any] = (token, intents) => Map("op" -> 2, "d" ->
     Map("token" -> s"Bot $token", "intents" -> intents, "properties" ->
       Map("$os" -> Constants.$os, "$browser" -> Constants.$browser, "$device" -> Constants.$device
       )
@@ -23,7 +23,7 @@ object PayloadModels {
   val heartbeatPayload: Map[String, Any] = Map("op" -> 1, "d" -> "null")
 
   // Resume payload in case the connection needs to be resumed.
-  val resumePayload: (String, String) => Map[Any, Any] = (token, sessionID) => Map("op" -> 6, "d" ->
+  val resumePayload: (String, String) => Map[String, Any] = (token, sessionID) => Map("op" -> 6, "d" ->
     Map("token" -> s"Bot $token", "session_id" -> sessionID, "seq" -> 7777)
   )
 }
