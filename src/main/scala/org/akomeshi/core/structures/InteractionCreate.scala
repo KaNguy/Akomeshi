959af4b2bf1c7ae782ad30ee5356b5132f935b8b
Yen <68307546+KaNguy@users.noreply.github.com>
2021-11-09 08:08:01 -0800

    Feature: Add `channelID` and `channel` for channel information and interaction.

--- src/main/scala/org/akomeshi/core/structures/InteractionCreate.scala
+++ src/main/scala/org/akomeshi/core/structures/InteractionCreate.scala
@@ -7,10 +7,13 @@ package org.akomeshi.core.structures
 
 // Akomeshi
 import org.akomeshi.utility.{Utilities => Util}
+import org.akomeshi.core.structures.Channel
 
 // Java Utilities
 import java.util
 
 case class InteractionCreate(interaction: util.HashMap[String, Any] = new util.HashMap[String, Any]()) {
   def applicationID: String = interaction.get("application_id").toString
+  def channelID: String = interaction.get("channel_id").toString
+  def channel: Channel = Channel(channelID)
 }
