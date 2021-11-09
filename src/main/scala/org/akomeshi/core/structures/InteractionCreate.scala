af89126983c109e762e8ccf07627a92bf86545c7
Yen <68307546+KaNguy@users.noreply.github.com>
2021-11-09 08:03:11 -0800

    Documentation: Add file heading.

--- src/main/scala/org/akomeshi/core/structures/InteractionCreate.scala
+++ src/main/scala/org/akomeshi/core/structures/InteractionCreate.scala
@@ -1,5 +1,10 @@
 package org.akomeshi.core.structures
 
+/**
+ * Created by KaNguy - 10/29/2021
+ * File org.akomeshi.core/structures/InteractionCreate.scala
+ */
+
 import java.util
 
 case class InteractionCreate(interaction: util.HashMap[String, Any] = new util.HashMap[String, Any]()) {
