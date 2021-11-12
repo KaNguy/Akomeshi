cb0fbdbe5e7ee8d9db82b2deb48bb4bb7d44f5d0
Yen <68307546+KaNguy@users.noreply.github.com>
2021-11-12 07:37:18 -0800

    Documentation: Revision spacing and syntax of documentation.

--- src/main/scala/org/akomeshi/utility/Utilities.scala
+++ src/main/scala/org/akomeshi/utility/Utilities.scala
@@ -15,26 +15,23 @@ import scala.collection.mutable.ListBuffer
 
 case object Utilities {
   /**
-   * Converts unknown data that is presumed to be a string into a boolean
-   *
-   * @param x Any, preferably a string-compatible parameter
-   * @return Boolean
+   * Converts unknown data that is presumed to be a string into a boolean.
+   * @param x Any, preferably a string-compatible parameter.
+   * @return Boolean.
    */
   def strToBool(x: Any): Boolean = x.toString.toBoolean
 
   /**
-   * Converts unknown data that is presumed to be a string into an integer
-   *
-   * @param x Any, preferably a string-compatible parameter
-   * @return Int
+   * Converts unknown data that is presumed to be a string into an integer.
+   * @param x Any, preferably a string-compatible parameter.
+   * @return Int.
    */
   def strToInt(x: Any): Int = x.toString.toInt
 
   /**
-   * Converts an object or weak reference HashMap directly into a HashMap
-   *
-   * @param x Preferably the assumed HashMap
-   * @return HashMap[String, Any]
+   * Converts an object or weak reference HashMap directly into a HashMap.
+   * @param x Preferably the assumed HashMap.
+   * @return HashMap[String, Any].
    */
   def toHashMap(x: Any): util.HashMap[String, Any] = x.asInstanceOf[util.HashMap[String, Any]]
 
@@ -43,10 +40,9 @@ case object Utilities {
 
   /**
    * Converts a Discord snowflake into a human-readable date
-   *
    * @see [[https://discord.com/developers/docs/reference#snowflakes]]
-   * @param snowflake Long
-   * @return Date in the form of a String
+   * @param snowflake Long.
+   * @return Date in the form of a String.
    */
   def snowFlakeToDate(snowflake: Long): String = {
     if (!snowflake.isValidLong) return snowflake.toString
@@ -55,11 +51,10 @@ case object Utilities {
   }
 
   /**
-   * Returns a list of user flags from an int
-   *
+   * Returns a list of user flags from an int.
    * @see [[https://discord.com/developers/docs/resources/user#user-object-user-flags]]
-   * @param flags Integer of the user flags
-   * @return A list of user flags
+   * @param flags Integer of the user flags.
+   * @return A list of user flags.
    */
   def getUserFlags(flags: Int): List[String] = {
     val userFlags: ListBuffer[String] = ListBuffer[String]()
@@ -70,11 +65,10 @@ case object Utilities {
   }
 
   /**
-   * Parses intents from an iterable collection into an integer
-   *
+   * Parses intents from an iterable collection into an integer.
    * @see [[https://discord.com/developers/docs/topics/gateway#gateway-intents]]
-   * @param intents List of intents in a single-value collection
-   * @return Intents integer
+   * @param intents List of intents in a single-value collection.
+   * @return Intents integer.
    */
   def parseIntents(intents: Iterable[String]): Int = {
     var intentsInt: Int = 0
